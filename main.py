3aa6982c3edc651b3f714fa25ee2f6f110d8f1df
Vagrant Default User <vagrant@control>
2019-10-21 10:14:41 +0000

    correct the file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+"""ceci est un programme"""
 from flask import Flask
-app=Flask(__name__)
-@app.route('/')
+APP = Flask(__name__)
+@APP.route('/')
 def hellowroldhander():
+    """ceci est la fonction"""
     return 'HEllo world from Flask & VAGRANT'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
+    APP.run(host="0.0.0.0")
